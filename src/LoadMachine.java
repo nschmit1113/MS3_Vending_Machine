3e1667fe8249ec8dc44fe2423b7add474e444c1e
nschmit1113 <schmittnikolaus1990@gmail.com>
2021-09-22 18:25:10 -0400

    Progress on JSON Reading

--- src/LoadMachine.java
+++ src/LoadMachine.java
@@ -1,9 +1,26 @@
 package src;
+
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 /**
  * This class will be responsible for loading the vending machine with the
  * items from the JSON file. 
  */
 
 public class LoadMachine {
-    
+
+    public LoadMachine(){
+        
+    }
+
+    public void readInJson(){
+        JSONParser jP = new JSONParser();
+        try {
+            JSONObject jsonObject = (JSONObject) jP.parse(new FileReader("../input.json"));
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
 }
